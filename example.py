9dd4bcafc3f11c15f70da732fe697f90d3def43d
Yaoyu Yang <yaoyu.yang88@gmail.com>
2016-06-07 17:48:53 -0700

    update

--- example.py
+++ example.py
@@ -1,5 +1,5 @@
 from cowfish import Cowfish
-co = Cowfish("http://aquarium_url/api", "user_name", 'your_api_key',r'/path/to/your_folder')
+co = Cowfish("http://aquarium_url/api", "user_name", 'your_api_key', r'/path/to/your_folder')
 
 df_singlets = co.cytometry_results_summary([20454,20472,20475,20476],ploidy="haploid", only='singlets')
 print df_singlets
